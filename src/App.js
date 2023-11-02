53308904a703df10279604e73b8722984c535d21
Anthony Fort <ajf2195@columbia.edu>
2023-11-02 18:11:20 +0000

    Removes redundant code from app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-import { useEffect } from 'react'
-import axios from 'axios'
-
-import { Routes, Route } from 'react-router-dom'
-
 import About from './components/About'
 import Contact from './components/Contact'
 import Hero from './components/Hero'
@@ -11,13 +6,6 @@ import Nav from './components/Nav'
 import Projects from './components/Projects'
 
 const App = () => {
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const { data } = await axios.get('/api/products/') // * <-- replace with your endpoint
-  //     console.log(data)
-  //   }
-  //   getData()
-  // })
 
   return (
     <>
@@ -30,7 +18,6 @@ const App = () => {
         <Contact />
       </main>
     </>
-
 
   )
 }
