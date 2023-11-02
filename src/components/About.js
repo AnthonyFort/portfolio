d29c85cb7171cef36a7743a078d2d05a14a98530
Anthony Fort <ajf2195@columbia.edu>
2023-11-02 13:58:07 +0000

    Adds icons to skills list

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,59 @@
-export default function About () {
-  return <h1>About</h1>
+import { Container, Row, Col } from 'react-bootstrap'
+
+
+
+export default function About() {
+  return (
+    <Container>
+      <Row>
+        <h1>About</h1>
+        <Col>
+          <p>I completed 420 hours of <strong>Software Engineering</strong> training through General Assembly. During that time, I was offered a place on a government-funded Skills Bootcamp in <strong>Cloud Engineering</strong>, run by AI Core, which I am currently completing.</p>
+          <p>I am currently open to work, so please check out my projects below and get in touch if you would like to talk more.</p>
+          <p>And if you&#39;re feeling curious, why not check out my music while you&#39;re here!</p>
+        </Col>
+        <h3>Skills</h3>
+        <div className="skills-icons-container">
+          <Col x3={3} md={2} lg={1} className="skills-icons-col">
+            <i className="devicon-python-plain skills-icons"></i>
+            <p>Python</p>
+          </Col>
+          <Col x3={3} md={2} lg={1} className="skills-icons-col">
+            <i className="devicon-javascript-plain skills-icons"></i>
+            <p>JavaScript</p>
+          </Col>
+          <Col x3={3} md={2} lg={1} className="skills-icons-col">
+            <i className="devicon-react-original skills-icons"></i>
+            <p>React</p>
+          </Col>
+          <Col x3={3} md={2} lg={1} className="skills-icons-col">
+            <i className="devicon-bootstrap-plain skills-icons"></i>
+            <p>Bootstrap</p>
+          </Col>
+          <Col x3={3} md={2} lg={1} className="skills-icons-col">
+            <i className="devicon-django-plain skills-icons"></i>
+            <p>Django</p>
+          </Col>
+          <Col x3={3} md={2} lg={1} className="skills-icons-col">
+            <i className="devicon-mongodb-plain skills-icons"></i>
+            <p>MongoDB</p>
+          </Col>
+          <Col x3={3} md={2} lg={1} className="skills-icons-col">
+            <i className="devicon-express-original skills-icons"></i>
+            <p>Express</p>
+          </Col>
+        </div>
+
+
+
+
+
+
+
+
+
+
+      </Row>
+    </Container>
+  )
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,14 @@
+import { Container, Row, Col } from 'react-bootstrap'
+
 export default function Hero () {
-  return <h1>Hero</h1>
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Anthony Fort</h1>
+          <h2>Software Engineer with a background in Music, Education, and Research</h2>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
