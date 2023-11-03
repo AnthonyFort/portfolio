ae7d504e2473d2a4b67d31110218b6034874f2f5
Anthony Fort <ajf2195@columbia.edu>
2023-11-03 10:26:52 +0000

    Improves right margin

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,47 +1,46 @@
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button, Carousel } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import evenNote from '../images/even-note.png'
 import phd from '../images/phd.png'
 import masters from '../images/masters.png'
 import podcast from '../images/podcast.png'
 
-
 export default function Music() {
   return (
-    <Container>
+    <Container >
       <h1>Music</h1>
-      <Row className='projects-container mb-4'>
-        <Col xs={12} md={6} className='image-div'>
+      <Row className='mb-4'>
+        <Col xs={12} md={6} >
           <img src={phd} alt='PHD' className='projects-image' />
         </Col>
-        <Col xs={12} md={6} className='music-text-div'>
+        <Col xs={12} md={5} >
           <h3>PhD Research</h3>
           <p>At Columbia University, I made videos which demonstrated how you can take a piece of music, and, by moving your body in very particular ways, change your subjective experience of the musical rhythm.</p>
           <p>My work in this area evolved a lot <i>after</i> writing the PhD; so, to read a more up-to-date essay and view the videos, please contact me.</p>
         </Col>
       </Row>
-      <Row className='projects-container mb-4'>
-        <Col xs={12} md={6} className='image-div'>
+      <Row className='mb-4'>
+        <Col xs={12} md={6} >
           <img src={evenNote} alt='even-note' className='projects-image' />
         </Col>
-        <Col xs={12} md={6} className='music-text-div'>
+        <Col xs={12} md={5} >
           <h3>Even-Note Illusion</h3>
           <p>As an appendix to my PhD, I constructed an auditory illusion. In it, I layered carefully-placed click sounds on top of a recording of a piano. The rhythm of the <i>piano</i> seems to change, quite dramatically.</p>
           <p>Please contact me for more info.</p>
         </Col>
       </Row>
-      <Row className='projects-container mb-4'>
-        <Col xs={12} md={6} className='image-div'>
+      <Row className='mb-4'>
+        <Col xs={12} md={6} >
           <img src={masters} alt='masters' className='projects-image' />
         </Col>
-        <Col xs={12} md={6} className='music-text-div'>
+        <Col xs={12} md={5} >
           <h3>Musical Decoding</h3>
           <p>Before I was a coder, I &apos;decoded&apos; a hidden pattern in a large orchestral work, <i>Palimpsests</i>, by the contemporary English Classical composer George Benjamin. This was for my Masters thesis (Columbia University, 2015).  Years later, I would realise that musical &apos;Set Theory&apos; and &apos;Sets&apos; in computer programming must be related!</p>
           <p>Please contact me for more info.</p>
         </Col>
       </Row>
-      <Row className='projects-container mb-4'>
-        <Col xs={12} md={6} className='image-div'>
+      <Row className='mb-4'>
+        <Col xs={12} md={6} >
           <iframe
             width="560"
             height="315"
@@ -51,13 +50,13 @@ export default function Music() {
             allowfullscreen
           ></iframe>
         </Col>
-        <Col xs={12} md={6} className='music-text-div'>
+        <Col xs={12} md={5} >
           <h3>Musical Composition</h3>
           <p>I&apos;m an avid composer and songwriter who loves to work in different genres. I&apos;m fascinated by how - for better or worse - my personality and self-beliefs leak into everything I do.</p>
         </Col>
       </Row>
-      <Row className='projects-container mb-4'>
-        <Col xs={12} md={6} className='image-div'>
+      <Row className='mb-4'>
+        <Col xs={12} md={6} >
           <iframe
             width="560"
             height="315"
@@ -67,13 +66,13 @@ export default function Music() {
             allowfullscreen
           ></iframe>
         </Col>
-        <Col xs={12} md={6} className='music-text-div'>
+        <Col xs={12} md={5} >
           <h3>Children&apos;s Music</h3>
           <p>Before coding, I had over 40 piano students, most of them children. For the little ones, I made fun music videos.</p>
         </Col>
       </Row>
-      <Row className='projects-container mb-4'>
-        <Col xs={12} md={6} className='image-div'>
+      <Row className='mb-4'>
+        <Col xs={12} md={6} >
           <iframe
             width="560"
             height="315"
@@ -83,21 +82,39 @@ export default function Music() {
             allowfullscreen
           ></iframe>
         </Col>
-        <Col xs={12} md={6} className='music-text-div'>
+        <Col xs={12} md={5} >
           <h3>University Teaching</h3>
           <p>Before coding, I taught Classical composition at the University of Cambridge. To supplement my teaching, I put together a 30-lesson course in Classical improvisation.</p>
         </Col>
       </Row>
-      <Row className='projects-container mb-4'>
-        <Col xs={12} md={6} className='image-div'>
+      <Row className='mb-4'>
+        <Col xs={12} md={6} >
           <img src={podcast} alt='masters' className='projects-image' />
         </Col>
-        <Col xs={12} md={6} className='music-text-div'>
+        <Col xs={12} md={5} >
           <h3>Psychology for Songwriters Podcast</h3>
-          <p>I love podcasts, but I could never find that provided a real deep-dive into the creative process. So I tried making one myself.</p>
+          <p>I love podcasts, but I could never find one that provided a real deep-dive into the creative process. So I tried making one myself.</p>
           <Button as={Link} to='https://podcasts.apple.com/us/podcast/psychology-for-songwriters/id1665379292' target='_blank' className='m-2'>Listen Here</Button>
         </Col>
       </Row>
+      <Row className='mb-4'>
+        <Col xs={12} md={6} >
+          <iframe
+            width="560"
+            height="315"
+            className='projects-image'
+            src="https://www.youtube.com/embed/59uGN0oxS6A"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </Col>
+        <Col xs={12} md={5} >
+          <h3>Union Chapel</h3>
+          <p>When I&apos;m not coding, I&apos;m the Director of Music at Union Chapel Church, Islington.</p>
+          <p>It&apos;s busy, but once CM Database has a few more users, I could save a few hours every week.</p>
+        </Col>
+      </Row>
     </Container >
+    
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,25 +6,25 @@ import Froggus from '../images/Froggus.png'
 export default function Projects() {
   return (
     <Container>
-      <h1>Projects</h1>
-      <Row className='projects-container mb-4'>
-        <Col xs={12} md={6} className='image-div'>
+      <h1>Coding Projects</h1>
+      <Row className='mb-4'>
+        <Col xs={12} md={6} >
           <img src={cmDatabaseImage} alt='CM database' className='projects-image' />
         </Col>
-        <Col xs={12} md={6} className='music-text-div'>
+        <Col xs={12} md={5} >
           <h3>CM Database</h3>
           <p>A time-saving tool for church musicians who need to find music related to a particular Bible reading or religious theme.</p>
           <Button as={Link} to='https://github.com/AnthonyFort/CM-Database-2#readme' target='_blank' className='m-2'>README</Button>
           <Button as={Link} to='https://cm-database-a7b05e162636.herokuapp.com/' target='_blank' className='m-2'>Try the app</Button>
         </Col>
       </Row>
-      <Row className='projects-container mb-4'>
-        <Col xs={12} md={6} className='image-div'>
+      <Row className='mb-4'>
+        <Col xs={12} md={6} >
           <img src={Froggus} alt='Froggus' className='projects-image' />
         </Col>
-        <Col xs={12} md={6} className='music-text-div'>
+        <Col xs={12} md={5} >
           <h3>Froggus Ad Parnassum</h3>
-          <p>An arcade-style game that teaches the fundamentals of Classical music improvisation (Google Chrome required).</p>
+          <p>An arcade-style game that teaches the fundamentals of Classical music improvisation (DESKTOP ONLY - Google Chrome required).</p>
           <Button as={Link} to='https://github.com/AnthonyFort/Froggus-ad-Parnassum/blob/main/README.md' target='_blank' className='m-2'>README</Button>
           <Button as={Link} to='https://anthonyfort.github.io/Froggus-ad-Parnassum/' target='_blank' className='m-2'>Play the Game</Button>
         </Col>
