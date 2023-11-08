5e538969d92448323240a144fe068d78a468e3e9
Anthony Fort <ajf2195@columbia.edu>
2023-11-08 18:06:04 +0000

    Improves styling for project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,34 +1,36 @@
 import { Container, Row, Col, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import cmDatabaseImage from '../images/CM-database.png'
-import Froggus from '../images/Froggus.png'
+import cmDatabaseImage from '../images/macbook-cm-database.png'
+import Froggus from '../images/macbook-froggus.png'
 
 export default function Projects() {
   return (
     <Container>
-      <h2 className='header-text'>Coding Projects</h2>
-      <Row className='mb-4'>
-        <Col xs={12} md={6} >
-          <img src={cmDatabaseImage} alt='CM database' className='projects-image' />
-        </Col>
-        <Col xs={12} md={5} >
-          <h3>CM Database</h3>
-          <p>A time-saving tool for church musicians who need to find music related to a particular Bible reading or religious theme.</p>
-          <Button as={Link} to='https://github.com/AnthonyFort/CM-Database-2#readme' target='_blank' className='m-2' size='sm'>README</Button>
-          <Button as={Link} to='https://cm-database-a7b05e162636.herokuapp.com/' target='_blank' className='m-2' size='sm'>Visit</Button>
-        </Col>
-      </Row>
-      <Row className='mb-4'>
-        <Col xs={12} md={6} >
-          <img src={Froggus} alt='Froggus' className='projects-image' />
-        </Col>
-        <Col xs={12} md={5} >
-          <h3>Froggus Ad Parnassum</h3>
-          <p>An arcade-style game that teaches the fundamentals of Classical music improvisation (DESKTOP ONLY - Google Chrome required).</p>
-          <Button as={Link} to='https://github.com/AnthonyFort/Froggus-ad-Parnassum/blob/main/README.md' target='_blank' className='m-2' size='sm'>README</Button>
-          <Button as={Link} to='https://anthonyfort.github.io/Froggus-ad-Parnassum/' target='_blank' className='m-2' size='sm'>Play the Game</Button>
-        </Col>
-      </Row>
+      <h2 className='header-text project-header'>Coding Projects</h2>
+      <div className='project-div'>
+        <Row className='mb-4 project-row'>
+          <Col xs={12} md={6} className='project-image-col' >
+            <img src={cmDatabaseImage} alt='CM database' className='projects-image' />
+          </Col>
+          <Col xs={12} md={8} lg={8} className='project-text-col'>
+            <h3>CM Database</h3>
+            <p>A search tool for church musicians to find music related to Bible readings or religious themes.</p>
+            <Button as={Link} to='https://github.com/AnthonyFort/CM-Database-2#readme' target='_blank' className='m-2' size='sm'>README</Button>
+            <Button as={Link} to='https://cm-database-a7b05e162636.herokuapp.com/' target='_blank' className='m-2' size='sm'>Visit</Button>
+          </Col>
+        </Row>
+        <Row className='mb-4 project-row'>
+          <Col xs={12} md={6} className='project-image-col' >
+            <img src={Froggus} alt='Froggus' className='projects-image' />
+          </Col>
+          <Col xs={12} md={8} className='project-text-col' >
+            <h3>Froggus Ad Parnassum</h3>
+            <p>A game that teaches musical theory (desktop - Chrome required).</p>
+            <Button as={Link} to='https://github.com/AnthonyFort/Froggus-ad-Parnassum/blob/main/README.md' target='_blank' className='m-2' size='sm'>README</Button>
+            <Button as={Link} to='https://anthonyfort.github.io/Froggus-ad-Parnassum/' target='_blank' className='m-2' size='sm'>Play</Button>
+          </Col>
+        </Row>
+      </div>
     </Container>
   )
 }
