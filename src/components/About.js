bca0dae094f5c956a72fdb6c0781dd81b972064e
Anthony Fort <ajf2195@columbia.edu>
2023-11-11 14:24:31 +0000

    Minor style changes

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,10 @@ export default function About() {
   return (
     <Container name='about' className='about-container' >
       <Row className='about-row'>
-        <Col xs={11} sm={8} md={7} lg={6} xl={5}>
+        <Col xs={11} sm={8} md={7} lg={6} xl={6}>
           <h2>About</h2>
-          <p>I am currently on a government-funded <strong>Cloud Engineering</strong> bootcamp with AI Core. Before that, I completed 420 hours of <strong>Software Engineering</strong> training at General Assembly. </p>
-          <p>I love coding and am open to working in any sector, so please check out my <Link as={Link} to="projects" smooth={true} duration={500} className='link-text'>projects</Link> below and <Link as={Link} to="contact" smooth={true} duration={500} className='link-text'>get in touch</Link> if you would like to talk more.</p>
+          <p>I am currently on a government-funded <strong>Cloud Engineering</strong> bootcamp with AI Core. Previously, I completed 420 hours of <strong>Software Engineering</strong> training at General Assembly. </p>
+          <p>I am open for work, so please feel free to <Link as={Link} to="contact" smooth={true} duration={500} className='link-text'>get in touch</Link>.</p>
         </Col>
       </Row>
     </Container>
