6cae6d4570a77fabf34e21b2c0633a0c40a8f358
Anthony Fort <ajf2195@columbia.edu>
2023-11-11 14:39:21 +0000

    Removes About heading

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About() {
     <Container name='about' className='about-container' >
       <Row className='about-row'>
         <Col xs={11} sm={8} md={7} lg={6} xl={6}>
-          <h2>About</h2>
+          <h2 style={{ color: 'white' }}>.</h2>
           <p>I am currently on a government-funded <strong>Cloud Engineering</strong> bootcamp with AI Core. Previously, I completed 420 hours of <strong>Software Engineering</strong> training at General Assembly. </p>
           <p>I am open for work, so please feel free to <Link as={Link} to="contact" smooth={true} duration={500} className='link-text'>get in touch</Link>.</p>
         </Col>
