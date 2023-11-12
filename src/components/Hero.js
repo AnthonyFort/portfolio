6a1f318313bfccb681859bd03947675bde36ed0c
Anthony Fort <ajf2195@columbia.edu>
2023-11-12 18:14:37 +0000

    Added photo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,29 @@
 import { Container, Row, Col } from 'react-bootstrap'
+import WebsitePic from '../images/website-pic-2.png'
 
-export default function Hero () {
+export default function Hero() {
   return (
-    <Container className='hero'>
-      <Row>
-        <Col>
-          <h1 className='hero-header'>Anthony Fort</h1>
-          <h3 className='hero-subheader'>Software Engineer</h3>
-          <h4 className='hero-subheader'>musician / teacher / researcher</h4>
-        </Col>
-      </Row>
-    </Container>
+    <div className='hero'>
+      <div>
+        <img src={WebsitePic} alt='Website picture' className='hero-image' />
+      </div>
+      <div>
+        <h1 className='hero-header'>Anthony Fort</h1>
+        <h3 className='hero-subheader'>Software Engineer</h3>
+        <h4 className='hero-subheader'>musician / teacher / researcher</h4>
+      </div>
+    </div>
+    // <Container className='hero'>
+    //   <Row>
+    //     <Col>
+    //       <img src={WebsitePic} alt='Website picture' className='hero-image' />
+    //     </Col>
+    //     <Col>
+    //       <h1 className='hero-header'>Anthony Fort</h1>
+    //       <h3 className='hero-subheader'>Software Engineer</h3>
+    //       <h4 className='hero-subheader'>musician / teacher / researcher</h4>
+    //     </Col>
+    //   </Row>
+    // </Container>
   )
 }
